48c5551b8eac0ec21a98a6313149b4c055c81a34
duduskz <101922498+duduskz@users.noreply.github.com>
2023-07-28 17:17:16 +0800

    添加功能，修复bug，独立全息

--- src/main/java/cn/lemoncraft/bedwars/API/Game.java
+++
@@ -1,9 +0,0 @@
-package cn.lemoncraft.bedwars.API;
-
-import java.util.ArrayList;
-
-public class Game {
-    public static ArrayList<String> getPlayers(String name) {
-        return null;
-    }
-}
